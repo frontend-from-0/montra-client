ecfcacb3b13ba5e9537027501db0287ad6c602ff
Savas Pamuk <savaspamuk@gmail.com>
2024-03-14 20:28:55 +0100

    Make Paper div a shared component once needed somewhere else

--- src/modules/onboarding/introduction/index.js
+++ src/modules/onboarding/introduction/index.js
@@ -1,19 +1,18 @@
-import React from "react";
 import SignUpButton from "../../../shared-components/SignUpButton";
 import LoginButton from "../../../shared-components/LoginButton";
 import { Stack } from "@mui/material";
-import "./introduction.css";
+
 import Slider from "./Slider";
+import { Paper } from "../../../shared-components/Paper/Paper";
 
 export const Introduction = () => {
-  // TODO: Make Paper div a shared component once needed somewhere else
   return (
-    <div className="paper">
+    <Paper>
       <Slider />
       <Stack spacing={2} direction="column">
         <SignUpButton />
         <LoginButton />
       </Stack>
-    </div>
+    </Paper>
   );
 };
