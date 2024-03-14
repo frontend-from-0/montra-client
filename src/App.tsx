17e787a9dbb529276fe8b446c696a0a2f4a7d0d9
Anna S. A <anna.atalay@minna.tech>
2024-03-14 22:35:51 +0100

    feat: update theme and add project colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import { AppRouter } from './AppRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme';
 
 export const App = () => {
   return (
     <div className='App'>
-      <AppRouter />
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
     </div>
   );
 };
---
+++ src/theme.tsx
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+import { colors } from './colors';
+
+export const theme = createTheme({
+  typography: {
+    h2: {
+      lineHeight: '34px',
+      fontSize: '32px',
+      fontWeight: 700,
+    },
+  },
+  palette: {
+    primary: {
+      main: colors.violet[100],
+      contrastText: colors.light[80],
+    },
+    secondary: {
+      main: colors.violet[20],
+      contrastText: colors.violet[100],
+    },
+    error: { main: colors.red[100], contrastText: colors.light[80] },
+    success: {
+      main: colors.green[100],
+      contrastText: colors.light[80],
+    },
+  },
+  shape: {
+    borderRadius: 12,
+  },
+});
