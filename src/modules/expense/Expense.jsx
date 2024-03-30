202f655087c5be57a58542f123e7a1001da96328
Saffet01 <saffetr.kaban@gmail.com>
2024-03-31 01:19:05 +0300

    feat: Expense Component

--- src/modules/expense/Expense.jsx
+++ src/modules/expense/Expense.jsx
@@ -2,25 +2,63 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export const Expense = () => {
   const theme = useTheme();
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 1,
-          width: '100%',
-          height: 700,
-          bgcolor: theme.palette.error.main,
-          borderRadius: 0,
-        },
-      }}
-    >
-      <Paper elevation={2}></Paper>
-    </Box>
+    <>
+      <Box
+        sx={{
+          height: '910px',
+          backgroundColor: 'black',
+        }}
+      >
+        <Box
+          sx={{
+            height: '900px',
+            backgroundColor: '#FD3C4A',
+          }}
+        >
+          <Typography>
+            <DateTime></DateTime>
+          </Typography>
+          <Paper
+            className='paper'
+            sx={{
+              borderRadius: '30px 30px 0 0',
+              height: '500px',
+              // marginBottom: 0,
+              // mt: '23.5rem',
+              mb: 0,
+            }}
+            elevation={2}
+          >
+            Saffet
+          </Paper>
+        </Box>
+      </Box>
+    </>
   );
 };
+
+export const DateTime = () => {
+  var [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000);
+
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <div>
+      <p>Time: {date.toLocaleDateString()}</p>
+    </div>
+  );
+};
+
+export default DateTime;
