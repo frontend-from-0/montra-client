5f6b2a6957bb7f0df2d40dadc7b63651a9345234
Saffet01 <saffetr.kaban@gmail.com>
2024-05-05 00:15:05 +0300

    fix: Shared component (Title3) used for header

--- src/modules/expense/Header.tsx
+++ src/modules/expense/Header.tsx
@@ -1,6 +1,7 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { styled, Theme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import { Title3 } from '@shared-components/Title3';
 import { colors } from 'src/styles/colors';
 
 const StyledDivHeader = styled(`div`)(({ theme }: { theme: Theme }) => ({
@@ -15,9 +16,7 @@ export const Header = () => {
   return (
     <StyledDivHeader>
       <ArrowBackIcon fontSize='large' sx={{ color: 'white' }} />
-      <Typography variant='h5' sx={{ color: 'white' }}>
-        Expense
-      </Typography>
+      <Title3>Expense</Title3>
       <Typography style={{ color: colors.red[100] }}>s</Typography>
     </StyledDivHeader>
   );
