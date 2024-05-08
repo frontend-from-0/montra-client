0e1169a3a77e42009b5b934ebc30659a52a8980f
Savas Pamuk <savaspamuk@gmail.com>
2024-05-08 11:53:47 +0200

    add form inputs

--- src/modules/onboarding/signup/index.tsx
+++ src/modules/onboarding/signup/index.tsx
@@ -1,8 +1,22 @@
-import { Stack, Button } from '@mui/material';
-import Link from '@mui/material/Link';
+import {
+  Stack,
+  Button,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  InputAdornment,
+  IconButton,
+  Typography,
+} from '@mui/material';
 import { Paper } from '../../../shared-components/Paper/Paper';
 import { styled, Theme } from '@mui/material/styles';
 import { Regular1 } from '../../../shared-components/Regular1';
+import GoogleIcon from '@mui/icons-material/Google';
+import React, { useState } from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Link from '@mui/material/Link';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 
 const StyledDiv = styled(`div`)(({ theme }: { theme: Theme }) => ({
   display: 'flex',
@@ -12,31 +26,87 @@ const StyledDiv = styled(`div`)(({ theme }: { theme: Theme }) => ({
 }));
 
 export const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <Paper>
       <StyledDiv>
-        <Stack spacing={2} direction='column'>
-          <Button
-            color='primary'
-            variant='contained'
-            size='large'
-            sx={{ textTransform: 'none' }}
-            disableElevation
+        <Stack direction='row' alignItems='center' spacing={1} sx={{ mb: 2 }}>
+          <IconButton
+            aria-label='introduction'
+            component='a'
+            href='/onboarding/introduction'
           >
+            <ArrowBack />
+          </IconButton>
+          <Typography variant='h5' component='h1'>
             Sign Up
-          </Button>
-          <Regular1>Or with</Regular1>
-          <Button
-            color='secondary'
-            variant='outlined'
-            size='large'
-            sx={{ color: 'black', textTransform: 'none' }}
-            disableElevation
-          >
-            Sign Up with Google
-          </Button>
-          <Regular1>Already have an account? Login</Regular1>
+          </Typography>
         </Stack>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={2} direction='column'>
+            <TextField label='Name' variant='outlined' required />
+            <TextField label='Email' type='email' variant='outlined' required />
+            <TextField
+              label='Password'
+              type={showPassword ? 'text' : 'password'}
+              variant='outlined'
+              required
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge='end'
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControlLabel
+              control={<Checkbox required />}
+              label='By signing up, you agree to the Terms of Service and Privacy Policy'
+            />
+            <Button
+              type='submit'
+              color='primary'
+              variant='contained'
+              size='large'
+              sx={{ textTransform: 'none' }}
+              disableElevation
+            >
+              Sign Up
+            </Button>
+            <Regular1>Or with</Regular1>
+            <Button
+              startIcon={<GoogleIcon />}
+              color='secondary'
+              variant='outlined'
+              size='large'
+              sx={{ color: 'black', textTransform: 'none' }}
+              disableElevation
+            >
+              Sign Up with Google
+            </Button>
+            <Regular1>Already have an account? Login</Regular1>
+          </Stack>
+        </form>
       </StyledDiv>
     </Paper>
   );
