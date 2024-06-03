64ab28bbcc00dc282a813344572c44750fc64803
saffet ramazan kaban <89699627+Saffet01@users.noreply.github.com>
2024-06-03 10:18:05 +0300

    MNTR-3 Sign up flow Redux store (#11)
    * feat: Redux Store Implemented
    
    * fix: Names are fixed
    
    * feat: Category and Frequency used as enum
    
    * fix: Some variables moved to component state
    
    * fix: Removed unused initial states and comment lines
    
    * fix: Reducers names are changed
    
    * fix: Prettier run
    
    * fix: import fixing
    
    * fix: Variables' names changed. Redux Component updated

---
+++ src/types/Category.tsx
@@ -0,0 +1,17 @@
+export enum Category {
+  utilities = 'Utilities',
+  food = 'Food',
+  Transportation = 'Transportation',
+  healthcare = 'Healthcare',
+  insurance = 'Insurance',
+  personal = 'Personal',
+  spending = 'Spending',
+  savings = 'Savings',
+  education = 'Education',
+  donation = 'Donation',
+  gifts = 'Gifts',
+  childcare = 'Childcare',
+  travel = 'Travel',
+  subscription = 'Substriction',
+  loan = 'Loan',
+}
